5f5c581459cf45b083e0004fcf58d68abaa9b40a
Akanksha Vyavhare <akankshavyavahare22@gmail.com>
2023-05-31 14:43:25 +0530

    quantity

--- ecommerce/src/Pages/Users/Addcart.js
+++ ecommerce/src/Pages/Users/Addcart.js
@@ -1,15 +1,24 @@
+import axios from 'axios';
 import React, { useContext, useState } from 'react';
 import { Appcart } from '../../App';
 
 const Addcart = (props) => {
     const { increaseCart, cartarray } = useContext(Appcart); // accessing from parent app.js through to context api
 
-    const addcartHandler = () => {
+    const addcartHandler = async () => {
         increaseCart(props.itemdetail);
         console.log(props.itemdetail);
-          let ls = localStorage.getItem("userobj")
-          console.log(ls);
-
+        let ls = localStorage.getItem("userobj")
+        console.log(ls);
+        // let obj ={
+        //     "CartId": "",
+        //     "CustId": "",
+        //     "ProductId": "",
+        //     "Quantity": "",
+        //     "AddedDate": Date(),
+        // }
+        // let res = await axios.post("http://onlinetestapi.gerasim.in/api/Ecomm/AddToCart",obj)
+        // console.log(res);
     }
 
     return (
@@ -17,21 +26,22 @@ const Addcart = (props) => {
         <div>
             <button className="btn btn-primary" onClick={() => addcartHandler()} > Add To Cart</button>
 
-            {cartarray.map((item)=>{
-                return( 
+            {cartarray.map((item) => {
+                return (
                     <div className='col-4'>
-                    <div className="card mt-5" style={{ width: "18rem" }}>
-                        <img className="card-img-top" src={item.productImageUrl} alt="camera" />
-                        <div className="card-body">
-                            <h5 className="card-title">Product Name{item.productName}</h5>
-                            <p className="card-text">qty{item.qty}</p>
-                            <p className="card-text">Price{item.productPrice}</p>
+                        <div className="card mt-5" style={{ width: "18rem" }}>
+                            <img className="card-img-top" src={item.productImageUrl} alt="camera" />
+                            <div className="card-body">
+                                <h5 className="card-title">Product Name{item.ProductId}</h5>
+                                <p className="card-text">qty{item.qty}</p>
+                                <p className="card-text">Rs {item.productPrice * item.qty}</p>
+                                <p className='card-text'> {item.AddedDate}</p>
 
+                            </div>
                         </div>
                     </div>
-                </div>
                 )
-            })}  
+            })}
         </div>
     );
 };
